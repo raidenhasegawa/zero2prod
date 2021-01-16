00be5286e88f336984d6d1ceb8be05274bad13e2
raidenhasegawa <raiden@slightdata.com>
2021-01-15 23:05:59 -0500

    add subscriptions route (still broken as it returns 200 regardless of payload)

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            // new route in the table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
